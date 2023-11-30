56587e25f8a2484d26316ecaad9c44a0b3d715d9
EduardoFariaKruger <90162399+EduardoFariaKruger@users.noreply.github.com>
2023-11-30 15:01:52 -0300

    A

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -30,7 +30,7 @@ void inicializaTabuleiro(int tabuleiro[N][N]) {
 void imprimeTabuleiro(int tabuleiro[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("%c ", tabuleiro[i][j] ? 'Q' : '.');
+            printf("%c ", tabuleiro[i][j] ? '!' : ')');
         }
         printf("\n");
     }
